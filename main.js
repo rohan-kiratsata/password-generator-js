b0a3f31d0f76aeedb42fd284f187704fb637f6b1
Rohan Kiratsata <49232485+rohan-kiratsata@users.noreply.github.com>
2022-08-25 12:15:39 +0530

    bug fixed: Responsive screen fixed copy button fixed

--- main.js
+++ main.js
@@ -14,86 +14,88 @@ const UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
 const LOWERCASE_CODES = arrayFromLowToHigh(97, 122);
 const NUMBER_CODES = arrayFromLowToHigh(48, 57);
 const SYMBOL_CODES = arrayFromLowToHigh(33, 47)
-    .concat(arrayFromLowToHigh(58, 64))
-    .concat(arrayFromLowToHigh(91, 96))
-    .concat(arrayFromLowToHigh(123, 126));
+  .concat(arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHigh(123, 126));
 
 function arrayFromLowToHigh(low, high) {
-    const array = [];
-    for (let i = low; i <= high; i++) {
-        array.push(i);
-    }
-    return array;
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
 }
 
 let generatePassword = (
+  characterAmount,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+) => {
+  let charCodes = LOWERCASE_CODES;
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CODES);
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CODES);
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CODES);
+  const passwordCharacters = [];
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode =
+      charCodes[Math.floor(Math.random() * charCodes.length)];
+    passwordCharacters.push(String.fromCharCode(characterCode));
+  }
+  return passwordCharacters.join("");
+};
+
+// FIXED: DEPRECATED COPY METHOD
+// copybtnDOM.addEventListener("click", () => {
+//   const textarea = document.createElement("textarea");
+//   const passwordToCopy = resultDOM.innerText;
+//   // A Case when Password is Empty
+//   if (!passwordToCopy) return;
+//   // Copy Functionality
+//   textarea.value = passwordToCopy;
+//   document.body.appendChild(textarea);
+//   textarea.select();
+//   document.execCommand("copy");
+//   textarea.remove();
+//   alert("Password Copied to Clipboard");
+// });
+
+function copyToClipboard() {
+  let passStr = resultDOM.innerHTML
+  navigator.clipboard.writeText(passStr);
+  alert(`Copied ${passStr} to clipboard!`)
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const characterAmount = lengthDOM.value;
+  const includeUppercase = uppercaseDOM.checked;
+  const includeNumbers = numbersDOM.checked;
+  const includeSymbols = symbolsDOM.checked;
+  const password = generatePassword(
     characterAmount,
     includeUppercase,
     includeNumbers,
     includeSymbols
-  ) => {
-    let charCodes = LOWERCASE_CODES;
-    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CODES);
-    if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CODES);
-    if (includeNumbers) charCodes = charCodes.concat(NUMBER_CODES);
-    const passwordCharacters = [];
-    for (let i = 0; i < characterAmount; i++) {
-      const characterCode =
-        charCodes[Math.floor(Math.random() * charCodes.length)];
-      passwordCharacters.push(String.fromCharCode(characterCode));
-    }
-    return passwordCharacters.join("");
-  };
+  );
+  resultDOM.innerText = password;
+});
 
-  copybtnDOM.addEventListener("click", () => {
-    const textarea = document.createElement("textarea");
-    const passwordToCopy = resultDOM.innerText;
-    // A Case when Password is Empty
-    if (!passwordToCopy) return;
-    // Copy Functionality
-    textarea.value = passwordToCopy;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand("copy");
-    textarea.remove();
-    alert("Password Copied to Clipboard");
-  });
+function passStren() {
+  var x = document.getElementById("passLen").value;
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const characterAmount = lengthDOM.value;
-    const includeUppercase = uppercaseDOM.checked;
-    const includeNumbers = numbersDOM.checked;
-    const includeSymbols = symbolsDOM.checked;
-    const password = generatePassword(
-      characterAmount,
-      includeUppercase,
-      includeNumbers,
-      includeSymbols
-    );
-    resultDOM.innerText = password;
-  });
+  var n = Number(x);
 
-//   if(lengthDOM < 10){
-//       printStrPass.innerText = "Weak";
-//   }
-
-function passStren(){
-    var x = document.getElementById("passLen").value;
-
-    var n = Number(x);
-    // document.getElementById("printS").innerHTML = x;
-
-    if(n <= 6){
-        document.getElementById("printS").innerHTML = "Weak";
-    }
-    else if(n > 6 && n <=15){
-        document.getElementById("printS").innerHTML = "Medium";
-    }
-    else if(n > 15 && n <=25){
-        document.getElementById("printS").innerHTML = "Strong";
-    }
-    else if(n > 25){
-        document.getElementById("printS").innerHTML = "Unbelievable";
-    }
+  if (n <= 6) {
+    document.getElementById("printS").innerHTML = "Weak";
+  }
+  else if (n > 6 && n <= 15) {
+    document.getElementById("printS").innerHTML = "Medium";
+  }
+  else if (n > 15 && n <= 25) {
+    document.getElementById("printS").innerHTML = "Strong";
+  }
+  else if (n > 25) {
+    document.getElementById("printS").innerHTML = "Unbelievable";
+  }
 }
